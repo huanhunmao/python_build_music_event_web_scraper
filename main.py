36331923f91a86c70a4a036fe227b5e521c852e9
Marxu <fuhaojun@52tt.com>
2024-07-02 15:03:58 +0800

    feat: 存/读数据，并在不重复时 发邮件

--- main.py
+++ main.py
@@ -23,7 +23,30 @@ def extract(source):
     return value
 
 
+def send_email():
+    print('Send email !')
+
+
+# 存事件
+def store(extracted):
+    with open('data.txt', 'a') as file:
+        file.write(extracted + '\n')
+
+
+# 读存储的事件
+def read_store_data():
+    with open('data.txt', 'r') as file:
+        return file.read()
+
+
 if __name__ == '__main__':
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+
+    content = read_store_data()
+    if extracted != 'No upcoming tours':
+        # 有事件 and 不重复，才会存数据，发邮件
+        if extracted not in content:
+            store(extracted)
+            send_email()
