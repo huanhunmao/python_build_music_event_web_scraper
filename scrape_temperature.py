c43093c7926f5356f15995e5738f95db7dd43ccc
Marxu <fuhaojun@52tt.com>
2024-07-03 16:43:10 +0800

    feat: 1、将抓到的数据存入 数据库 db 文件； 2、读取 db 里的 参数 画图

--- scrape_temperature.py
+++ scrape_temperature.py
@@ -2,8 +2,12 @@ import requests
 import selectorlib
 from datetime import datetime
 import os
+import sqlite3
 
 URL = 'http://programmer100.pythonanywhere.com/'
+
+connection = sqlite3.connect('data1.db')
+cursor = connection.cursor()
 
 def scrape(url):
     response = requests.get(url)
@@ -20,14 +24,9 @@ def extract(source):
 
 def store_data(data):
     now = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
-    file_path = 'date_temperature.txt'
-    file_exists = os.path.exists(file_path)
-    with open('date_temperature.txt', 'a') as file:
-        # 不存在表头才添加，存在不重复添加
-        if not file_exists or os.stat(file_path).st_size == 0:
-            file.write('Date,Temperature' + '\n')
-        line = f'{now},{data}\n'
-        file.write(line)
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO temperatures VALUES(?,?)',(now,data))
+    connection.commit()
 
 
 if __name__ == '__main__':
--- webapp.py
+++ webapp.py
@@ -1,9 +1,22 @@
 import streamlit as st
 import plotly.express as px
-import pandas
+import sqlite3
 
-df = pandas.read_csv('date_temperature.txt')
+connection = sqlite3.connect('data1.db')
+cursor = connection.cursor()
 
-figure = px.line(x=df['Date'],y=df['Temperature'],labels={'x':'Date', 'y':'Temperature'})
+# 1 获取时间戳 date
+cursor.execute('SELECT timestamp FROM temperatures')
+timestamp = cursor.fetchall()
+# print(timestamp) # [('24-07-03-16-26-09',), ('24-07-03-16-26-18',), ('24-07-03-16-26-21',)]
+date = [item[0] for item in timestamp]
+# print(date) # ['24-07-03-16-26-09', '24-07-03-16-26-18', '24-07-03-16-26-21']
+
+# 2 获取 温度 temperature
+cursor.execute('SELECT temperature FROM temperatures')
+temperature = cursor.fetchall()
+temperature = [item[0] for item in temperature]
+
+figure = px.line(x=date,y=temperature,labels={'x':'Date', 'y':'Temperature'})
 
 st.plotly_chart(figure)
