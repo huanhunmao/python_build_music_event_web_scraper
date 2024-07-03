ef897813a5f9761b69a71606e2ae874d7c91aecc
Marxu <fuhaojun@52tt.com>
2024-07-03 19:58:47 +0800

    feat: 将抓取数据放在 一个类 Event 并使用这个类 ，注意 函数定义第一个参数为 self def scrape(self,url)

--- main.py
+++ main.py
@@ -11,23 +11,24 @@ URL = 'http://programmer100.pythonanywhere.com/tours/'
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
-def scrape(url):
-    response = requests.get(url)
-    source = response.text
-    return source
+class Event:
+        def scrape(self,url):
+            response = requests.get(url)
+            source = response.text
+            return source
 
 
-def extract(source):
-    # 从 extract.yaml 文件中创建 Extractor 对象
-    extractor = selectorlib.Extractor.from_yaml_file('extract.yaml')
+        def extract(self,source):
+            # 从 extract.yaml 文件中创建 Extractor 对象
+            extractor = selectorlib.Extractor.from_yaml_file('extract.yaml')
 
-    # 使用 Extractor 对象从 source 中提取数据
-    extracted_data = extractor.extract(source)
+            # 使用 Extractor 对象从 source 中提取数据
+            extracted_data = extractor.extract(source)
 
-    # 返回提取到的 tours 数据
-    value = extracted_data.get('tours')
+            # 返回提取到的 tours 数据
+            value = extracted_data.get('tours')
 
-    return value
+            return value
 
 # 存事件
 def store(extracted):
@@ -53,8 +54,9 @@ def read_store_data(extracted):
 
 if __name__ == '__main__':
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
 
         if extracted != 'No upcoming tours':
