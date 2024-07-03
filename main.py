6268f190dfc61e355110d7afce0086c5cac99a67
Marxu <fuhaojun@52tt.com>
2024-07-03 20:16:08 +0800

    feat: 新加一个类 Database， 并将公共要用的东西放在  def __init__(self, dbname):内;;self 是一个指向当前对象的引用，用于访问对象的属性和方法

--- main.py
+++ main.py
@@ -8,48 +8,49 @@ import sqlite3
 
 URL = 'http://programmer100.pythonanywhere.com/tours/'
 
-connection = sqlite3.connect('data.db')
-cursor = connection.cursor()
-
 class Event:
-        def scrape(self,url):
-            response = requests.get(url)
-            source = response.text
-            return source
+    def scrape(self,url):
+        response = requests.get(url)
+        source = response.text
+        return source
 
 
-        def extract(self,source):
-            # 从 extract.yaml 文件中创建 Extractor 对象
-            extractor = selectorlib.Extractor.from_yaml_file('extract.yaml')
+    def extract(self,source):
+        # 从 extract.yaml 文件中创建 Extractor 对象
+        extractor = selectorlib.Extractor.from_yaml_file('extract.yaml')
 
-            # 使用 Extractor 对象从 source 中提取数据
-            extracted_data = extractor.extract(source)
+        # 使用 Extractor 对象从 source 中提取数据
+        extracted_data = extractor.extract(source)
 
-            # 返回提取到的 tours 数据
-            value = extracted_data.get('tours')
+        # 返回提取到的 tours 数据
+        value = extracted_data.get('tours')
 
-            return value
+        return value
 
-# 存事件
-def store(extracted):
-    row = extracted.split(',')
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    print('row',row)
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    connection.commit()
+class Database:
+    def __init__(self, dbname):
+        self.connection = sqlite3.connect(dbname)
+
+    # 存事件
+    def store(self, extracted):
+        row = extracted.split(',')
+        row = [item.strip() for item in row]
+        cursor =  self.connection.cursor()
+        print('row',row)
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
 
 
-# 读存储的事件
-def read_store_data(extracted):
-    row = extracted.split(',')
-    row = [item.strip() for item in row]
-    band, city, date = row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
+    # 读存储的事件
+    def read_store_data(self,extracted):
+        row = extracted.split(',')
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor =  self.connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
 
 
 if __name__ == '__main__':
@@ -60,9 +61,10 @@ if __name__ == '__main__':
         print(extracted)
 
         if extracted != 'No upcoming tours':
-            row = read_store_data(extracted)
+            database = Database(dbname='data.db')
+            row = database.read_store_data(extracted)
             # 有事件 and 不重复，才会存数据，发邮件
             if  not row:
-                store(extracted)
+                database.store(extracted)
                 send_email(extracted)
         time.sleep(2)
